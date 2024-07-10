a673b4950af9faa15e283a4852ead2a0a94e8a7e
kash2005 <kashmifernando3@gmail.com>
2024-07-10 15:45:18 +0530

    init the project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from "@react-navigation/native"
+import {createNativeStackNavigator} from "@react-navigation/native-stack"
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Loading'>
+        <Stack.Screen name='Loading' component={LoadingPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ src/pages/LoadingPage.tsx
@@ -0,0 +1,9 @@
+import { View, Image } from "react-native";
+
+export default function LoadingPage() {
+    return(
+        <View>
+            <Image source={require('')}/>
+        </View>
+    )
+}
